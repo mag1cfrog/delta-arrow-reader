18c00566870961c8f173f0dac4bbcdb66e4f4484
mag1cfrog <harrywong2017@gmail.com>
2026-08-27 00:46:33 -0700

    refactor: keep a single crate version API

--- src/lib.rs
+++ src/lib.rs
@@ -31,8 +31,3 @@ pub use reader::{
 };
 /// The crate version.
 pub const VERSION: &str = env!("CARGO_PKG_VERSION");
-
-/// Returns the crate version.
-pub const fn version() -> &'static str {
-    VERSION
-}
